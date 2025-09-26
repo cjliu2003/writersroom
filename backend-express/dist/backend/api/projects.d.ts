823a10a70784d8f52f9c8989bce7de1649cafef3
jlofwall <125715978+jlofwall@users.noreply.github.com>
2025-09-26 08:11:18 -0700

    Final Refactor
    - Deleted express backend (using FastAPI)
    - Deleted unneeded frontend files and tests
    - Cleaned up code: no stray files; all in proper folders and directories

--- backend-express/dist/backend/api/projects.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from 'express';
-export declare function registerProject(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-export declare function listProjects(req: Request, res: Response): Promise<void>;
-export declare function deleteProject(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=projects.d.ts.map
